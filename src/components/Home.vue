c09a7237145a7b5b12969a0e5e59f9788226c5d6
刘龙宾 <liulongbin1314@outlook.com>
2021-02-22 10:57:15 +0800

    完成了登录和退出信息

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <el-button type="info" @click="loginout">退出</el-button>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        //退出登录
+        loginout() {
+            // 1. 销毁token值
+            window.sessionStorage.clear();
+            // 2. 重定向到登录页
+            this.$router.push('/login');
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
