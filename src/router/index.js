961648613b2c9c908c861a4abda3354d839cfb32
刘龙宾 <liulongbin1314@outlook.com>
2021-05-20 16:28:36 +0800

    完成了权限功能的开发！

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 import Welcome from '../components/welcome.vue'
 import Users from '../components/user/Users.vue'
+import Rights from '../components/power/Rights.vue'
+import Roles from '../components/power/Roles.vue'
 
 Vue.use(Router)
 
@@ -11,7 +13,16 @@ const router = new Router({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home, redirect: '/welcome', children: [{ path: '/welcome', component: Welcome }, { path: '/users', component: Users }] }
+    { path: '/home', 
+      component: Home, 
+      redirect: '/welcome', 
+      children: [
+        { path: '/welcome', component: Welcome }, 
+        { path: '/users', component: Users },
+        { path: '/rights', component:Rights },
+        { path: '/roles', component:Roles }
+      ] 
+    }
   ]
 })
 
